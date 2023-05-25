494e630a2f9844859a227cd6d73f7ba2f2b08474
Polina <polinakiseleva4@gmail.com>
2023-05-26 00:54:00 +0200

    Приведение кода в читабельный вид

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from transliterate import translit
 from config import config
 from employers.employers_parser import EmployersParser
 from vacancies.vacancies_parser import VacanciesParser
@@ -11,20 +12,19 @@ def main():
     print('Мы рады приветствовать вас!\n'
           'Эта программа предназначена для получения информации о работодателях\n'
           'и их вакансиях, а также для создания базы данных, которую можно будет\n'
-          'использовать для получения нужной информации.')
+          'использовать для получения нужной информации.\n')
 
     params = config()
 
-    employer_kw = input('Введите ключевое слово, по которому будет осуществляться'
+    employer_kw = input('Введите ключевое слово, по которому будет осуществляться\n'
                         'поиск работодателя: ')
     exp_emp = EmployersParser()
     employers_data = exp_emp.get_employer_data(employer_kw)
     employers_indexes = exp_emp.get_employer_id(employers_data)
 
-    print(''
-          'Ищем информацию о работодателях и их вакансиях...')
+    print('Ищем информацию о работодателях и их вакансиях...\n')
 
-    exp_emp.get_json_employers(employer_kw, employers_data)
+    exp_emp.get_json_employers(employer_kw)
 
     exp_vac = VacanciesParser()
     vacancies_data = exp_vac.get_employers_vacancies(employers_indexes)
@@ -32,21 +32,21 @@ def main():
 
     db_name = input('Для продолжения работы введите название базы данных: ').strip().replace(' ', '_')
     print(f'Создаем базу данных с названием {db_name}...')
-    print(f'Загружаем данные о работодателях и вакансиях в базу данных {db_name}...')
+    print(f'Загружаем данные о работодателях и вакансиях в базу данных {db_name}...\n')
 
     create_database(db_name, params)
     create_tables(db_name, params)
-    enter_data_into_emp_database(db_name, params, employers_data)
-    enter_data_into_vac_database(db_name, params, vacancies_data)
+    enter_data_into_emp_database(db_name, params)
+    enter_data_into_vac_database(db_name, params)
 
     db_manager = DBManager(db_name, config())
     user_input = input("Для продолжения работы выберите команду из списка:\n"
                        "1: Получение списка всех компаний и количества вакансий у каждой компании\n"
-                       "2: Получение списка всех вакансий с указанием названия компании,"
+                       "2: Получение списка всех вакансий с указанием названия компании, хир"
                        "названия вакансии и ссылки на вакансию\n"
-                       "3: Получение списка всех вакансий по ключевому слову \n"
-                       "4: Выход из программы \n"
-                       "   Введите цифру нужной команды ...   ").strip()
+                       "3: Получение списка всех вакансий по ключевому слову\n"
+                       "4: Выход из программы\n"
+                       "   Введите цифру нужной команды: ").strip()
 
     if user_input == '1':
         print(db_manager.get_companies_and_vacancies_count())
@@ -55,7 +55,7 @@ def main():
         print(db_manager.get_all_vacancies())
 
     elif user_input == '3':
-        vacancy_kw = input('Введите ключевое слово: ').strip()
+        vacancy_kw = input('   Введите ключевое слово: ').strip()
         print(db_manager.get_vacancies_with_keyword(vacancy_kw))
 
     elif user_input not in ('1', '2', '3', '4'):
